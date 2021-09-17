c75e09936d59e162b82da704d985afcb13a90b4f
haojen <haojenma@gmail.com>
2021-09-18 01:06:12 +0800

    Init project

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,15 +4,58 @@ module.exports = {
     node: true
   },
   extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/standard',
-    '@vue/typescript/recommended'
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/typescript/recommended"
   ],
   parserOptions: {
     ecmaVersion: 2020
   },
+  globals: {},
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+    "semi": [
+      "error",
+      "never"
+    ],
+    "quotes": [
+      "error",
+      "single",
+      {
+        "allowTemplateLiterals": true
+      }
+    ],
+    "eqeqeq": "error",
+    "no-else-return": "error",
+    "object-curly-spacing": [
+      "error",
+      "always"
+    ],
+    "space-infix-ops": "error",
+    "no-duplicate-imports": "error",
+    "no-var": "error",
+    "prefer-const": "warn",
+    "spaced-comment": [
+      "error",
+      "always"
+    ],
+    "indent": [
+      "error",
+      4,
+      {
+        "VariableDeclarator": "first",
+        "SwitchCase": 1
+      }
+    ]
+  },
+  overrides: [
+    {
+      "files": [
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)"
+      ],
+      "env": {
+        "jest": true
+      }
+    }
+  ]
 }
