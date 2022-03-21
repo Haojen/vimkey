374dec26ff4f93a71516c3aabb9f9a2c85ca24c1
Haojen <haojenma@gmail.com>
2022-03-21 04:37:02 +0000

    Deploying to gh-pages from  @ 3490d1ebc78f6ea29117967c7bd97b41d9955041 🚀

---
+++ precache-manifest.e4d1b82b6596745298858c3bb0a6165c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7f2d5fd2d8c2cbd9d28",
+    "url": "/vimkey/css/app.4dbf254f.css"
+  },
+  {
+    "revision": "261456415de3c2987868",
+    "url": "/vimkey/css/chunk-vendors.a6a2021e.css"
+  },
+  {
+    "revision": "4b8ed3a49ba9c7e54538",
+    "url": "/vimkey/css/support.f867f5c7.css"
+  },
+  {
+    "revision": "0f3514a45d51f95167e5fe8b6a03bb60",
+    "url": "/vimkey/img/Download_on_the_App_Store_Badge_US-UK_RGB_wht_092917.0f3514a4.svg"
+  },
+  {
+    "revision": "1290c17dcab8919875d080fecc904e0e",
+    "url": "/vimkey/img/Download_on_the_Mac_App_Store_Badge_US-UK_RGB_wht_092917.1290c17d.svg"
+  },
+  {
+    "revision": "d73435b5bd1506742b24ba9e71503056",
+    "url": "/vimkey/img/chrome-webstore.d73435b5.svg"
+  },
+  {
+    "revision": "00a2298be49884c6c8a8a99efef0b5a9",
+    "url": "/vimkey/img/guy-working-at-home-2127164-0.00a2298b.svg"
+  },
+  {
+    "revision": "3d984cc48cbe1a039bb772a297935ced",
+    "url": "/vimkey/img/logo.3d984cc4.png"
+  },
+  {
+    "revision": "0d711a03bf6b2b44504413fd9ed2ab8f",
+    "url": "/vimkey/index.html"
+  },
+  {
+    "revision": "55d722229df38443c89a",
+    "url": "/vimkey/js/about.2ee898d9.js"
+  },
+  {
+    "revision": "b7f2d5fd2d8c2cbd9d28",
+    "url": "/vimkey/js/app.092869a8.js"
+  },
+  {
+    "revision": "261456415de3c2987868",
+    "url": "/vimkey/js/chunk-vendors.154acb7d.js"
+  },
+  {
+    "revision": "4b8ed3a49ba9c7e54538",
+    "url": "/vimkey/js/support.73fe2149.js"
+  },
+  {
+    "revision": "d02b693a65e92214aa42133ce66246db",
+    "url": "/vimkey/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vimkey/robots.txt"
+  }
+]);
--- src/main.ts
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-
-createApp(App).use(router).mount('#app')
