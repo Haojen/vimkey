ca836b063cc481d78f4a557adaf113e57d90d5a2
Haojen <haojenma@gmail.com>
2022-11-23 00:07:15 +0800

    Update Privacy Policy

--- src/views/Privacy.vue
+++ src/views/Privacy.vue
@@ -1,15 +1,24 @@
 <template>
-    <main class="flex flex-col justify-center">
-        <p class="bg-gradient-to-b from-yellow-400 to-yellow-500 shadow rounded-2xl p-2 flex justify-center items-center"
-           style="width: 70px; height: 70px">
-            <svg fill="#fff" width="50" height="50" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="65057"><path d="M817.856928 588.065391c-17.563826 21.303652-58.494145 88.160464-73.638957 135.286725s-42.570203 92.605217-67.969855 146.558145-100.433623 119.54829-192.578783 124.022725-219.90029-10.388406-277.51884-78.914783-56.869101-150.201507-56.639073-197.298087c0.192928-40.811594 0.051942-133.565217 0-157.629217V249.84858a39.646609 39.646609 0 0 1 79.300638 0V532.99942a399.10771 399.10771 0 0 1 45.31571-16.881159V108.922435a45.31571 45.31571 0 0 1 90.624 0V498.643478a462.729275 462.729275 0 0 1 39.646609-1.81055V75.100754a45.31571 45.31571 0 0 1 90.624 0v430.013217c16.465623 3.027478 33.495188 7.056696 50.977391 12.050551V120.208696a45.31571 45.31571 0 0 1 90.624 0v452.222145c20.309333 15.293217 33.984928 26.913391 33.984928 26.913391s38.103188-81.022145 96.293101-118.375884 76.243478-35.090551 101.954783-5.63942-33.443246 91.432812-50.999652 112.736463z" p-id="65058"></path></svg>
-        </p>
-        <h1 class="text-2xl font-bold mt-2 border-gray-300 pb-0.5">Privacy Policy</h1>
-        <p class="text-lg tracking-tight">Vimkey No collect any privacy information, requested permissions are only used for the corresponding features.</p>
-        <p class="mt-6 text-gray-400">
-            When enabling Vimkey, you will be warned that the extension will have access to sensitive information. This access is required for the extension to be able to interact with the website. Vimkey never collects, stores, or transmits any information. It runs entirely locally on your device
+    <main class="flex flex-col justify-center text-center">
+        <div class="inline-block mx-auto relative">
+            <img src="../../public/assets/logo-256.png" alt="" style="width: 100px">
+            <i class="bg-gradient-to-b from-yellow-400 to-yellow-500 shadow rounded-2xl p-2 flex justify-center items-center absolute -right-3 -bottom-2"
+               style="width: 50px; height: 50px">
+                <svg fill="#fff" width="50" height="50" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="65057"><path d="M817.856928 588.065391c-17.563826 21.303652-58.494145 88.160464-73.638957 135.286725s-42.570203 92.605217-67.969855 146.558145-100.433623 119.54829-192.578783 124.022725-219.90029-10.388406-277.51884-78.914783-56.869101-150.201507-56.639073-197.298087c0.192928-40.811594 0.051942-133.565217 0-157.629217V249.84858a39.646609 39.646609 0 0 1 79.300638 0V532.99942a399.10771 399.10771 0 0 1 45.31571-16.881159V108.922435a45.31571 45.31571 0 0 1 90.624 0V498.643478a462.729275 462.729275 0 0 1 39.646609-1.81055V75.100754a45.31571 45.31571 0 0 1 90.624 0v430.013217c16.465623 3.027478 33.495188 7.056696 50.977391 12.050551V120.208696a45.31571 45.31571 0 0 1 90.624 0v452.222145c20.309333 15.293217 33.984928 26.913391 33.984928 26.913391s38.103188-81.022145 96.293101-118.375884 76.243478-35.090551 101.954783-5.63942-33.443246 91.432812-50.999652 112.736463z" p-id="65058"></path></svg>
+            </i>
+        </div>
+        <h1 class="text-2xl font-bold mt-2 border-gray-300 mt-4 mb-6">Privacy Policy</h1>
+        <p class="text-lg font-semibold tracking-tight text-gray-400 mx-auto" style="max-width: 640px">
+            <span class="text-white">
+                Vimkey No collect any privacy information,
+                requested permissions are only used for the corresponding features.
+            </span>
             <br>
-            in Safari, You can choose to allow just for one day or always and you can also choose to allow just on specific websites or on every website. You can revoke access at any time using Manage Extensions.
+            <span>
+                When enabling Vimkey, you will be warned that the extension will have access to sensitive information. This access is required for the extension to be able to interact with the website. Vimkey never collects, stores, or transmits any information. It runs entirely locally on your device
+                in Safari, You can choose to allow just for one day or always and you can also choose to allow just on specific websites or on every website. You can revoke access at any time using Manage Extensions.
+            </span>
+
         </p>
     </main>
 </template>
